41d1a96c623ee62383f743a8e33b638494a14c7f
Aleksander Lamkov <aleksanderlamkov@gmail.com>
2025-07-11 16:33:54 +0300

    refactor(cli): улучшен UX выбора шаблона и инициализации проекта

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ import { blue, cyan, green, bold } from 'kolorist'
 const showLogo = () => {
   console.log()
   console.log(cyan('╭────────────────────────────────────────────╮'))
-  console.log(cyan('│     ') + bold(blue('Friendly Frontend Starter CLI')) + cyan('     │'))
+  console.log(cyan('│') + bold(blue('     Friendly Frontend Starter CLI     ')) + cyan('│'))
   console.log(cyan('╰────────────────────────────────────────────╯'))
   console.log()
 }
